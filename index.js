22de78db70b063db7b722844b627590f0e892554
TE109 <1991tmce@gmail.com>
2024-09-29 18:11:46 -0400

    Index added endpoint & response

--- index.js
+++ index.js
@@ -6,6 +6,46 @@ const port = process.env.PORT || 3000;
 app.get('/',(req,res) => {
     res.send("Test");
 })
-app.listen(port, () =>{
-    // console.log("http://127.0.0.1:3000/");
-});
+
+
+app.post('/api/v1/user/signup',(req,res) => {
+    res.status(201);
+})
+
+
+
+app.post('/api/v1/user/login',(req,res) => {
+    res.status(200);
+})
+
+
+
+app.get('/api/v1/emp/employees',(req,res) => {
+    res.status(200);
+})
+
+
+
+app.post('/api/v1/emp/employees',(req,res) => {
+    res.status(201);
+})
+
+
+app.get('/api/v1/emp/employees/{eid}',(req,res) => {
+    res.status(200);
+})
+
+
+
+app.put('/api/v1/emp/employees/{eid}',(req,res) => {
+    res.status(200);
+})
+
+
+app.delete('/api/v1/emp/employees/{eid}',(req,res) => {
+    res.status(204);
+})
+
+app.listen(port, () => {
+    
+})
