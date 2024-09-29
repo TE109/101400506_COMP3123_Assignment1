e9b9c095effd0eccbf9a5098ed141eb1885762f1
TE109 <1991tmce@gmail.com>
2024-09-29 18:57:16 -0400

    mongoose Schemas added to index

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const UserSchema = mongoose.Schema(
+    {
+        "_id": ObjectId,
+        "username": String,
+        "email": String,
+        "password": String, // This should be hashed
+        "created_at": Date,
+        "updated_at": Date
+       }
+);
+
+const EmployeeSchema = mongoose.Schema(
+    {
+        "_id": ObjectId,
+        "first_name": String,
+        "last_name": String,
+        "email": String,
+        "position": String,
+        "salary": Number,
+        "date_of_joining": Date,
+        "department": String,
+        "created_at": Date,
+        "updated_at": Date
+       }       
+)
 
 app.get('/',(req,res) => {
     res.send("Test");
