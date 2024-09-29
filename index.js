92e491c393219276a1f141ec178394a0f8c6936a
TE109 <1991tmce@gmail.com>
2024-09-29 18:01:30 -0400

    Vercal Push

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.get('/',(req,res) => {
+    res.send("Test");
+})
+app.listen(port, () =>{
+    // console.log("http://127.0.0.1:3000/");
+});
